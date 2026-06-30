62ca9ea09678aeb09642e2a549ccb3095362d8a4
Raphael Rodrigues <professor.dev.raphael@gmail.com>
2026-06-30 19:33:18 -0300

    Preparando o arquivo Recaptcha para os alunos

---
+++ MoviesFullstackTerca.API/Services/RecaptchaService.cs
@@ -0,0 +1,16 @@
+﻿namespace MoviesFullstackTerca.API.Services;
+
+public static class RecaptchaService
+{
+    private static readonly HttpClient _httpClient = new();
+
+    public static async Task<(bool isValid, string reason)> ValidateToken(string token, string expectedAction)
+    {
+        // Adicionar o resto do código para validar o token do reCAPTCHA aqui
+        // ===========================================
+        // CODIGO AQUI
+        // ===========================================
+
+        return (true, "OK");
+    }
+}
