0fb662897d308658980a2495d91a1e679f87e694
Raphael Rodrigues <professor.dev.raphael@gmail.com>
2026-06-30 20:16:55 -0300

    Adiciona classe RecaptchaResponse ao RecaptchaService.cs
    Inclui a classe RecaptchaResponse para representar o resultado da verificação do reCAPTCHA, com propriedades como sucesso, score, ação, timestamp do desafio e hostname. Isso facilita o tratamento das respostas do reCAPTCHA na aplicação.

--- MoviesFullstackTerca.API/Services/RecaptchaService.cs
+++ MoviesFullstackTerca.API/Services/RecaptchaService.cs
@@ -14,3 +14,13 @@ public static class RecaptchaService
         return (true, "OK");
     }
 }
+
+public class RecaptchaResponse
+{
+    public bool success { get; set; } // Indica se a verificação do reCAPTCHA foi bem-sucedida
+    public double score { get; set; } // Pontuação atribuída pelo reCAPTCHA, indicando a probabilidade de a interação ser humana
+    public string action { get; set; } = string.Empty; // Ação associada à verificação do reCAPTCHA
+    public string challenge_ts { get; set; } = string.Empty; // Timestamp(Data) do desafio do reCAPTCHA
+    public string hostname { get; set; } = string.Empty; // Nome do host(Local) onde o reCAPTCHA foi executado
+
+}
